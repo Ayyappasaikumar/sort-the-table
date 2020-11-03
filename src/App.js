b6d33edd88b3804bc5085fb32326ba06c8500843
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-11-03 13:50:39 +0530

    sort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+  
+import React from 'react'
+import db from "./db.json"
+import schema from './schema';
+import Table from "./Table"
 function App() {
+function sortasc(){
+  db.sort(function(a, b){
+    return a.id - b.id;
+   
+});
+console.log(db);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-2">
+      <div className="row">
+        <div className="col">
+        
+          <Table headers={Object.keys(schema)} rows={db} />
+          <button  onClick={sortasc}>sort asc</button>
+        </div>
+      </div>
     </div>
   );
+   
+  
 }
 
 export default App;
+
+
+
+
