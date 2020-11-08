ff6f3f9206b722bb822d27054eb0c5545ccf7045
ayyappa sannidhi <ayyappasaikumar12@gmail.com>
2020-11-08 21:39:19 +0530

    sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
   
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import db from "./db.json"
 import schema from './schema';
 import Table from "./Table"
 function App() {
-function sortasc(){
-  db.sort(function(a, b){
-    return a.id - b.id;
-   
-});
-console.log(db);
-  }
+  const bands=db;
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState(db);
+
+  useEffect(() => {
+    
+     
+      const sorted = [...bands].sort((a, b) => a.id-b.id);
+      setData(sorted);
+    
+
+    
+  }, [bands]);
+console.log(bands)
   return (
     <div className="container p-2">
       <div className="row">
         <div className="col">
         
-          <Table headers={Object.keys(schema)} rows={db} />
-          <button  onClick={sortasc}>sort asc</button>
+          <Table headers={Object.keys(schema)} rows={data} />
+          <button onChange={(e) => setData(e.target.value)}>sort</button>
         </div>
       </div>
     </div>
